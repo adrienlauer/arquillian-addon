7b9c8b44bf2318bb6f6833f5f4bcafca7b50d566
Adrien LAUER <adrien.lauer@mpsa.com>
2017-12-21 18:21:30 +0100

    wip

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# arquillian-addon
-Provides support for Web integration testing through Arquillian.
+# SeedStack Arquillian add-on
+
+[![Build status](https://travis-ci.org/seedstack/arquillian-addon.svg?branch=master)](https://travis-ci.org/seedstack/arquillian-addon) [![Coverage Status](https://coveralls.io/repos/seedstack/arquillian-addon/badge.svg?branch=master)](https://coveralls.io/r/seedstack/arquillian-addon?branch=master) [![Maven Central](https://maven-badges.herokuapp.com/maven-central/org.seedstack.addons.arquillian/arquillian/badge.svg?style=flat)](https://maven-badges.herokuapp.com/maven-central/org.seedstack.addons.arquillian/arquillian)
+
+Official Arquillian integration for SeedStack.
+
+# Copyright and license
+
+This source code is copyrighted by [The SeedStack Authors](https://github.com/seedstack/seedstack/blob/master/AUTHORS) and
+released under the terms of the [Mozilla Public License 2.0](https://www.mozilla.org/MPL/2.0/). 
---
+++ tomcat/src/test/java/org/seedstack/arquillian/ArquillianIT.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright © 2013-2018, The SeedStack authors <http://seedstack.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package org.seedstack.arquillian;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.net.URL;
+import javax.inject.Inject;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.seedstack.seed.Application;
+
+@RunWith(Arquillian.class)
+public class ArquillianIT {
+    @Inject
+    private Application application;
+
+    @Deployment
+    public static WebArchive createDeployment() {
+        return ShrinkWrap.create(WebArchive.class);
+    }
+
+    @Test
+    @RunAsClient
+    public void arquillianIsWorking(@ArquillianResource URL baseUrl) {
+        assertThat(baseUrl).isNotNull();
+    }
+
+    @Test
+    @RunAsClient
+    public void injectionIsWorking(@ArquillianResource URL baseUrl) {
+        assertThat(application).isNotNull();
+    }
+}
